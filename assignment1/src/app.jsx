ae452080329f6841f5c7cda8c3b36102ba41b72b
hemanthsaichalla <hemanthsaichalla@gmail.com>
2020-02-02 19:44:19 -0800

    Initial commit
    Initial commit

---
+++ assignment1/src/app.jsx
@@ -0,0 +1,14 @@
+var nameNode = document.getElementById('intro');
+var component = <article classNameame="column">
+                <h2>Hemanth Sai Challa</h2>    
+                <img className="center" src="image/image1.jpg" alt="picture"></img>	
+			    <section>
+                <p>I am a Computer Science graduate Student at San Diego State University with extensive education in software design and development, experienced in
+                  automated testing and debugging code. Fluent in English with excellent communication and interpersonal skills.
+				  A fast learner with strong time management and problem-solving skills. The idea behind taking this course is, 
+				  my undergrad has laid a foundation in web technology fundamentals and now it's the interest to dwell deeper into the concepts has made me choose this course.
+			      I believe I have a decent knowledge in web concepts and by the end of this course I could get expertise in the MERN.</p>
+			  </section>
+			  <a href=" https://www.google.com/">View my GitHub Repository</a>
+		    </article>;
+ReactDOM.render(component, nameNode);
